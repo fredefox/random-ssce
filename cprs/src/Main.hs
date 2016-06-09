9d25d2bb00cf13e15a1c34f106742d2cca161c31
Frederik Hanghøj Iversen <frhi@netcompany.com>
2016-06-09 22:18:03 +0200

    Add ui for checking if cpr is an existing cpr-number

--- cprs/src/Main.hs
+++ cprs/src/Main.hs
@@ -1,11 +1,23 @@
 import Cpr
 import Text.Printf
 import System.Environment
+import LookupPerson
 
 main :: IO ()
 main = getArgs >>= mapM_ checkIsCpr
 
 checkIsCpr :: String -> IO ()
-checkIsCpr s = printf "%-8s %s\n" t s where
+checkIsCpr s = printStatus t s where
     t | validCpr s = "VALID"
       | otherwise  = "INVALID"
+
+checkIsExistingCpr :: String -> IO ()
+checkIsExistingCpr cpr = do
+    s <- lookupPerson cpr
+    let t = case s of
+            (Left _) -> "NOEXIST"
+            _        -> "EXIST"
+    printStatus t cpr
+
+printStatus :: PrintfType r => r
+printStatus = printf "%-8s %s\n"
