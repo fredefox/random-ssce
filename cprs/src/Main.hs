fc2e14340677ec38647963ba5509e5eee597a3b6
Frederik Hanghøj Iversen <frhi@netcompany.com>
2016-06-09 19:34:53 +0200

    Just check explicitly mentioned cpr-numbers

--- cprs/src/Main.hs
+++ cprs/src/Main.hs
@@ -1,5 +1,11 @@
 import Cpr
-import Control.Monad (forM_)
+import Text.Printf
+import System.Environment
 
 main :: IO ()
-main = forM_ cprs putStrLn
+main = getArgs >>= mapM_ checkIsCpr
+
+checkIsCpr :: String -> IO ()
+checkIsCpr s = printf "%-8s %s\n" t s where
+    t | validCpr s = "VALID"
+      | otherwise  = "INVALID"
